7ec6aea4c01f38fc0715868c8fe0762eeeb6340f
vickylian <2274738535@qq.com>
2019-01-25 14:30:23 +0800

    ldd - 登录

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,15 +1,47 @@
 import React from 'react'
-import banner from '../../static/img/login-banner.png'
+import {
+  Form, Icon, Input, Button,
+} from 'antd';
+import AppHeader from '../../components/AppHeader'
+import AppFooter from '../../components/AppFooter'
+import './style.css'
+import $ from 'jquery'
+
+
 
 class Login extends React.Component {
 
-  state = {}
-
+  state = {
+    activeIndex:0,
+  }
+  //切换tab
+  selectTab(item,index){
+    this.activeIndex = index;
+    $('.tab_box span').removeClass('activeTab').eq(index).addClass('activeTab')
+  }
+  callback(key) {
+    console.log(key);
+  }
   render () {
     return (
       <div>
-        <img src={banner} width="100%" alt=""/>
-        这是Login组件
+       <AppHeader />
+        <div className='content_box'>
+          <div className='login_box'>
+             <div className='right_box'>
+               <div className='title'>
+                <span className='activeTitle'>登录</span>
+                <span>注册</span>
+               </div>
+              <div className='tab_box'>
+                <span className='activeTab'>密码登录</span>
+                <span >验证码登录</span>
+              </div>
+              
+             </div>
+          </div>
+        </div>
+        <AppFooter />
       </div>
     )
   }
