392cccccd9b9c8d51b13dccd10ad44b09725fdb6
ldq <513993984@qq.com>
2019-01-25 17:48:18 +0800

    ldq - 网站首页和商城页面

--- src/routes/Mall/index.jsx
+++ src/routes/Mall/index.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import { Button } from 'antd'
+import banner from '../../static/img/mall-banner.png'
+import AppTitle from '../../components/AppTitle'
+import AppCardList from '../../components/AppCardList'
+
 
 class Mall extends React.Component {
 
-  state = {}
+  state = {
+    goods: ['男性专区', '女性专区', '儿童必备', '中药饮品', '当季热销']
+  }
 
   render () {
     return (
       <div>
-        这是Mall组件
+        <img src={banner} width="100%" alt=""/>
+        {
+          this.state.goods.map(item => {
+            return (
+              <div className="wrap">
+                <AppTitle>{item}</AppTitle>
+                <AppCardList></AppCardList>
+                <div style={{ textAlign: 'center', margin: '30px' }}>
+                  <Button className="ldq-btn">查看更多</Button>
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
