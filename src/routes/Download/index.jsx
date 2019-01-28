40a2ef45c402e05fd1ecec38d509f7b5e0075e3a
vickylian <2274738535@qq.com>
2019-01-28 10:43:40 +0800

    ldd - 资料下载

--- src/routes/Download/index.jsx
+++ src/routes/Download/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-
+import './style.css'
 class Download extends React.Component {
 
   state = {}
 
   render () {
     return (
-      <div>
-        这是Download组件
+      <div class='download_box'> 
+        <div class='data'>
+           <h1 class='data_title'>【备 案 资 料】</h1>
+           <ul>
+             <li>
+               <p>经营企业（批发企业、零售连锁药店、药店）</p>
+               
+             </li>
+           </ul>
+        </div>
+       
       </div>
     )
   }
